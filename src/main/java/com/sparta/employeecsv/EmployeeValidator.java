0dc6bb96869f113c9e2b4c58c8694da695879c36
HarryLewis955 <84735551+HarryLewis955@users.noreply.github.com>
2022-02-09 17:26:38 +0000

    Validation test complete email done

--- src/main/java/com/sparta/employeecsv/EmployeeValidator.java
+++ src/main/java/com/sparta/employeecsv/EmployeeValidator.java
@@ -3,6 +3,7 @@ package com.sparta.employeecsv;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.*;
 
 public class EmployeeValidator {
     private static HashMap<Integer, Employee> uniqueEmployees;
@@ -13,26 +14,33 @@ public class EmployeeValidator {
     }
 
 
-    public boolean validate(Employee emp) {
+    public static boolean validate(Employee emp) {
         if (validateNamePrefix(emp.getPrefix()) && validateGender(emp.getGender()) && validateEmail(emp.getEmail())){
             return true;
         };
     return false;
     }
-    public boolean validateNamePrefix(String prefix){
+    private static boolean validateNamePrefix(String prefix){
         if (prefix.equals("Mrs.") || prefix.equals("Mr.") || prefix.equals("Dr.")|| prefix.equals("Hon.")|| prefix.equals("Ms.") || prefix.equals("Drs.") || prefix.equals("Prof.")){
             return true;
         }
         return false;
     }
-    public boolean validateGender(char gender){
+    private static boolean validateGender(char gender){
         if (gender == 'M' || gender == 'F' || gender == 'X'){
             return true;
         }
         return false;
     }
-    public boolean validateEmail(String email){
-        return true;
+    private static boolean validateEmail(String email) {
+        boolean result = true;
+        Pattern regexEmailPattern = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+        Matcher matcher = regexEmailPattern.matcher(email);
+        boolean correct = matcher.find();
+        if (correct) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean isUnique(Employee emp){
