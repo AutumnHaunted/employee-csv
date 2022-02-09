7ea05610f16ae90df3a73a5455a936a4d5907974
Autumn Pelešová <autumnlara@hotmail.com>
2022-02-09 17:35:23 +0000

    Moved Sajad's pseudo main code to main, not yet updated/refactored

--- src/main/java/com/sparta/employeecsv/Main.java
+++ src/main/java/com/sparta/employeecsv/Main.java
@@ -1,8 +1,34 @@
 package com.sparta.employeecsv;
 
 public class Main {
+
+
     public static void main(String[] args) {
         EmployeeList el = CSVReadHandler.readCSV(DisplayHandler.getFilePath());
         DisplayHandler.printEmployeeLists(el);
+
+        logger.info("Program started");
+        long startTotal = System.currentTimeMillis();
+        System.out.println("Reading in .csv file...");
+        long startReadFile = System.currentTimeMillis();
+
+        // EmployeeList employeeList = CSVReadHandler.readValues("src/main/resources/EmployeeRecordsLarge.csv");
+        System.out.println("Time taken to read in data: " + (System.currentTimeMillis() - startReadFile) + " ms");
+
+        logger.info("Time taken to read in data from csv: " + (System.currentTimeMillis() - startReadFile) + " ms");
+
+        System.out.println("Filtering .csv file...");
+        long startFilter = System.currentTimeMillis();
+        //employeeList.filterEmployees();
+
+        System.out.println("\tNumber of valid employee records: " + employeeList.getEmployeeList().size());
+        System.out.println("\tTime taken to filter out invalid data: " + (System.currentTimeMillis() - startFilter) + " ms");
+
+        //employeeList.removeAll(invalidList);
+        System.out.println("\tAmount of duplicate records found: " + invalidList.size());
+        logger.info("Amount of duplicate records found: " + invalidList.size());
+
+        System.out.println("Writing invalid records to InvalidEmployeeRecords.csv...");
+        // employeeList.writeInvalidToFile("src/main/resources/InvalidEmployeeRecords.csv");
     }
 }
